c534845cc54a941b922fb68327abfddfa200a0db
nguyen <cuong.nguyen@crossian.com>
2022-10-04 18:19:45 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+exports.port = process.env.PORT || 5000
+exports.mongo = {
+    uri: `mongodb+srv://clolc:cuong179@cluster0.i1cff.mongodb.net/clolc?retryWrites=true&w=majority`,
+    options: {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+}
+exports.jwtSecetKey = process.env.JWT_SECRET_KEY || cuong12345
